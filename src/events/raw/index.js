912dd7f826dec93d83cd4c0759ef7d87e1d6a77f
João Victor <joaovictor-san-co@hotmail.com>
2020-11-14 16:57:37 -0300

    inicio do comando que aprova ou desaprova os reports

--- src/events/raw/index.js
+++ src/events/raw/index.js
@@ -1,7 +1,78 @@
 const client = require('../..');
+const { static: { eID, emoji } } = require('../../utils/emojis.json');
+const { report_analise, report_reprovados, report_aprovados, admins } = require('../../config/default.json');
+const api = require('../../services/api');
+const moment = require('moment');
 
-module.exports = (data) => {
-  if (data.t === 'MESSAGE_REACTION_ADD') {
-    console.log(data.d);
+module.exports = async (data) => {
+  const { error, apiError } = require('../../functions');
+
+  if (
+    data.t === 'MESSAGE_REACTION_ADD' &&
+    data.d.channel_id === report_analise &&
+    data.d.user_id !== client.user.id &&
+    admins.includes(data.d.user_id)
+  ) {
+    client.channels.cache.get(report_analise).messages.fetch(data.d.message_id)
+      .then(message => {
+        const channelAprovado = client.channels.cache.get(report_aprovados);
+        const channelReprovado = client.channels.cache.get(report_reprovados);
+        const reportId = Number(message.embeds[0].title);
+    
+        if (message.author.id !== client.user.id) return;
+    
+        if (data.d.emoji.id === eID.emojicoffeecheck) {
+          api.post('/reports/update', { reports: { status: [{ id: reportId, value: 'APROVADO' }] } })
+            .then(() => {
+              channelAprovado.send(message.embeds[0])
+                .then(msg => {
+                  msg.react(eID.emojicoffeecheck);
+
+                  api.get(`/reports/${reportId}`)
+                    .then(response => {
+                      client.users.fetch(response.data.created_by)
+                        .then(user => {
+                          user.send(
+                            `> Seu report "${response.data.report}" feito ${moment(response.data.created_timestamp).locale('pt-br').format('LLLL')} foi `)
+                        });
+                    });
+    
+                  message.delete();
+                }, e => sendError(e));
+            }, e => updateErr(e));
+        } else {
+          api.post('/reports/update', { reports: { status: [{ id: reportId, value: 'REPROVADO' }] } })
+            .then(() => {
+              channelReprovado.send(message.embeds[0])
+                .then(msg => {
+                  message.delete();
+                }, e => sendError(e));
+            }, e => updateErr(e));
+        };
+      }, e => error(
+        `> ${emoji.emojicoffeeinfo} Aviso!\n` +
+        '> Não foi possível encontrar a mensagem que continha o report para aprova-la ou reprova-la!\n' +
+        `> Path: "${__filename}"\n` +
+        `> ID da mensagem: "${data.d.message_id}"\n` +
+        `> Erro: "${JSON.stringify(e, null, 4)}"`
+      ));
+  };
+
+  function updateErr(e) {
+    error(
+      `> ${emoji.emojicoffeeerro} Erro\n` +
+      '> Houve um erro ao atualizar o status de um report!\n' +
+      `> Path: "${__filename}"\n` +
+      `> Erro: "${apiError(e)}"`
+    )
+  };
+
+  function sendError(e) {
+    error(
+      `> ${emoji.emojicoffeeinfo} Aviso!\n` +
+      '> Não foi possível enviar o report para o canal destinado!\n' +
+      `> Path: "${__filename}"\n` +
+      `> Erro: "${JSON.stringify(e, null, 4)}"`
+    )
   };
 };

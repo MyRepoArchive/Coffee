bf44fce75e759fdda1db513b2c94809b1981f38b
João Victor <joaovictor-san-co@hotmail.com>
2020-12-30 17:19:32 -0300

    update

--- src/commands/fun/grau/index.js
+++ src/commands/fun/grau/index.js
@@ -2,7 +2,7 @@ module.exports = {
   config: require('./src/config'),
 
   run({ message, permissions }) {
-    if (permissions.has('SEND_MESSAGES')) message.channel.send('RANDAMDAM', { files: ['./src/assets/audios/randandanPA.mp3'] }).then(() => {
+    if (permissions.has('SEND_MESSAGES')) message.channel.send('RANDAMDAM').then(() => {
       setTimeout(() => message.channel.send('PA').catch(() => {}), 2000)
     }, () => {});
   }

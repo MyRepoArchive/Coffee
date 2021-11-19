a726038940b2dbd5abab6c001aa2bd48c6458460
João Victor <joaovictor-san-co@hotmail.com>
2021-11-19 10:45:53 -0300

    remove dist folder

--- dist/utils/getFormatedDirname.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getFormatedDirname(dirname) {
-    return dirname.endsWith('\\')
-        ? dirname.replace(/\\/g, '/')
-        : dirname.replace(/\\/g, '/') + '/';
-}
-exports.default = getFormatedDirname;

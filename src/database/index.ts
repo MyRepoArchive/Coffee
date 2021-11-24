3c1031b60b1d4f86730a8b46d0af2ef166ea57da
João Victor <joaovictor-san-co@hotmail.com>
2021-11-24 12:14:56 -0300

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,22 @@
 import { connection } from '..'
+import log from '../utils/log'
 import ChannelsController from './controllers/ChannelsController'
 import GuildsController from './controllers/GuildsController'
 
 export type DatabaseTables = 'channels' | 'guilds'
 
 export default class Database {
-  guilds: GuildsController
-  channels: ChannelsController
+  guilds!: GuildsController
+  channels!: ChannelsController
 
   constructor() {
-    connection.getConnection(() => {})
-    this.guilds = new GuildsController(connection)
-    this.channels = new ChannelsController(connection)
+    connection.getConnection((err, connection) => {
+      if (err)
+        return log.error('Erro ao estabelecer conexão pool:', {
+          restLogs: [err],
+        })
+      this.guilds = new GuildsController(connection)
+      this.channels = new ChannelsController(connection)
+    })
   }
 }
